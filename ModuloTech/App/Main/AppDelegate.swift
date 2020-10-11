74d782a6dcd1ca7c7c7fbc98fd69440a8e644d3f
Ahmed Kaouach <akaouach-ext@lequipe.fr>
2020-10-11 13:03:02 +0200

    First config

---
+++ ModuloTech/App/Main/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ModuloTech
+//
+//  Created by Ahmed K. on 11/10/2020.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
